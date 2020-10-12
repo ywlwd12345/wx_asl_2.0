2d9f342de4a8bca52c71a1202a487faa305f4435
ywl <2979869953@qq.com>
2020-10-12 17:12:56 +0800

    我的会员

--- src/untils/icon.js
+++ src/untils/icon.js
@@ -1,1 +1,3 @@
 export const addressUrlIcon = 'https://w.aishenglian.com/static/icon/zwy-icon/dz.png' //小程序平台地址 图标
+export const backIcon = 'https://w.aishenglian.com/static/icon/ver2-icon/level-2/pb/fh@3x.png' //小程序平台2.0返回按钮
+export const avatar = 'https://w.aishenglian.com/static/icon/ver2-icon/my-ver2/tx@2x.png' //小程序平台2.0 默认头像
